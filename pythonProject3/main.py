47bc9fb71a2c4d338f465b1492bd88818d3b3705
lolyou1202 <sasha555990@gmail.com>
2022-11-20 22:12:25 +0300

    орлблдож

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -19,8 +19,6 @@ def login():
     record = cursor.fetchall()
     return render_template('account.html', full_name=record)
 
-def sanya():
-    print('Хуем полбу не дало???')
 
 if __name__ == "__main__":
     app.run(debug=True)
