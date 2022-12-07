54b696d52a2d9a9457dbc225eac6aa6cbcad81ae
lolyou1202 <sasha555990@gmail.com>
2022-12-07 22:37:12 +0300

    12333

--- pythonProject3/frontend/app/static/js/connect.js
+++ pythonProject3/frontend/app/static/js/connect.js
@@ -12,30 +12,16 @@
 //   });
 
 
-
-//  // функция будет принимать в качестве параметра URL
-//  async function getUsers() {
-//  // Отправление запроса к тестовому URL
-//     const response = await fetch('/add');
-//  // Получение ответа от сервера в формате json
-//     const answer = await response.json();
-//  // Выводим в консоль переменную answer
-//     // console.log(answer);
-//     return answer;
-//  }
-
 const getUsers = async () => {
    let response = await fetch('/add');
    if (response.ok) {
       let data = await response.json();
-      document.querySelector('.dashboard__main').innerHTML = data[1];
+      document.querySelector('.dashboard__main').innerHTML = data;
    }
 }
  // Повесим на кнопку обработчик событий
-  const button = document.querySelectorAll('.dashboard__single-button').forEach((el) => {
-    el.addEventListener('click', (e) => {
-      getUsers();
-    });
-  })
-
-//fetch('/add').then(response => response.json()).then(data => console.log(data));
+const button = document.querySelectorAll('.dashboard__single-button').forEach((el) => {
+   el.addEventListener('click', (e) => {
+   getUsers();
+   });
+})
