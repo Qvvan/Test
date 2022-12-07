20eb0b9966f35781dcbda0ea6226e5f92a9b071b
lolyou1202 <sasha555990@gmail.com>
2022-12-07 21:42:06 +0300

    промисы надо изучать

--- pythonProject3/frontend/app/static/js/connect.js
+++ pythonProject3/frontend/app/static/js/connect.js
@@ -13,26 +13,29 @@
 
 
 
- // функция будет принимать в качестве параметра URL
- async function getUsers() {
- // Отправление запроса к тестовому URL
-    const response = await fetch('/add');
- // Получение ответа от сервера в формате json
-    const answer = await response.json();
- // Выводим в консоль переменную answer
-    // console.log(answer);
-    return answer;
- }
- 
+//  // функция будет принимать в качестве параметра URL
+//  async function getUsers() {
+//  // Отправление запроса к тестовому URL
+//     const response = await fetch('/add');
+//  // Получение ответа от сервера в формате json
+//     const answer = await response.json();
+//  // Выводим в консоль переменную answer
+//     // console.log(answer);
+//     return answer;
+//  }
+
+const getUsers = async () => {
+   let response = await fetch('/add');
+   if (response.ok) {
+      let data = await response.json();
+      document.querySelector('.dashboard__main').innerHTML = data[1];
+   }
+}
  // Повесим на кнопку обработчик событий
- setTimeout(() => {
   const button = document.querySelectorAll('.dashboard__single-button').forEach((el) => {
     el.addEventListener('click', (e) => {
-  
-      document.querySelector('.dashboard__main').innerHTML = getUsers();
-      console.log(getUsers());
+      getUsers();
     });
   })
-}, 1000);
 
-fetch('/add').then(response => response.json()).then(data => console.log(data));
+//fetch('/add').then(response => response.json()).then(data => console.log(data));
