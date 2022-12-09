72caf03b423dc86282671283ff9a16f5bedc3e45
lolyou1202 <sasha555990@gmail.com>
2022-12-10 00:29:52 +0300

    finaly 09.12

--- pythonProject3/frontend/app/static/js/connect.js
+++ pythonProject3/frontend/app/static/js/connect.js
@@ -6,31 +6,21 @@ const getUsers = async () => {
     }
 }
 
-const buttonAdd = document.querySelector('.dashboard__single-button.add')
+const buttonAdd = document.querySelector('.dashboard__single-button.discard')
     .addEventListener('click', (e) => {
 
    });
-const buttonEdit = document.querySelector('.dashboard__single-button.edit')
-    .addEventListener('click', (e) => {
-        getUsers();
-    });
-const buttonDelete = document.querySelector('.dashboard__single-button.delete')
-    .addEventListener('click', (e) => {
-        getUsers();
-    });
 
 getUsers()
     .then((data) => {
-        const table = document.querySelector('.main__table');
-        const tbody = document.createElement('tbody');
-        const tr = document.createElement('tr');
-        tr.classList.add('main__tr');
+        const table = document.querySelector('.table');
+        const tableRow = document.createElement('div');
+        tableRow.classList.add('table__row')
         for (let i = 1; i <= 8; i++) {
-            const td = document.createElement('td');
-            td.textContent = data[i - 1][1];
-            tr.appendChild(td);
+            const cell = document.createElement('span');
+            cell.textContent = data[i - 1][1];
+            tableRow.appendChild(cell);
         }
-        table.appendChild(tbody);
-        tbody.appendChild(tr);
+        table.appendChild(tableRow);
         console.log(data);
     })
