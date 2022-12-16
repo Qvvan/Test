44dd4ab6f43d0d9eaa63be756726df26eafc3afb
Qvvan <ivan_ivan_lebedev@mail.ru>
2022-12-17 02:04:29 +0300

    update

--- pythonProject3/backend/FDataBasa.py
+++ pythonProject3/backend/FDataBasa.py
@@ -3,7 +3,7 @@ class FDataBase:
         self.__db = db
         self.__cursor = db.cursor()
 
-    def menu(self):
+    def menu(self) -> dict:
         quest = "SELECT json_agg(json_build_object('id', p.id, 'name', p.name, 'code', p.code, 'unit', p.unit, 'count', pm.count,"\
 				"'price_purchase', p.price_purchase , 'price_selling', p.price_selling))"\
                 "FROM public.product_market as pm JOIN public.product p ON pm.product_id = p.id;"
@@ -18,9 +18,13 @@ class FDataBase:
         return []
 
 
-    def order(self, column, order):
+    def order(self, column: int, order: str) -> dict:
         self.__column = column
         self.__order = order
+        if self.__order == 'Возрастание':
+            self.__order = 'ASC'
+        else:
+            self.__order = 'DESC'
         if self.__column == 'count':
             quest = "SELECT json_agg(json_build_object('id', p.id, 'name', p.name, 'code', p.code, 'unit', p.unit, 'count', pm.count," \
                     f"'price_purchase', p.price_purchase , 'price_selling', p.price_selling) ORDER BY pm.{self.__column} {self.__order})" \
