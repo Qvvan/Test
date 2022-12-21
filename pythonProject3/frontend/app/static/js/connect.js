db39eb28d89a4801714db990f3a90b5739d52499
lolyou1202 <sasha555990@gmail.com>
2022-12-21 23:31:30 +0300

    21e41234

--- pythonProject3/frontend/app/static/js/connect.js
+++ pythonProject3/frontend/app/static/js/connect.js
@@ -139,14 +139,31 @@ document.querySelector('.btn__modal-decision.ok').addEventListener('click', () =
     let modalContent = {
         'Наименование': name,
         'Артикул': article,
-        'Тип': type,
-        'Уже существующий товар': product,
+        //'Тип': type,
+        //'Уже существующий товар': product,
         'Количество': count,
         'Единицы': units,
         'Причина списания': reason,
         'id': idSave
     }
     console.log(modalContent);
-    postUsers(modalContent, '/add').then(asdaad => console.log(asdaad));
+    postUsers(modalContent, '/add').then((data) => {
+        
+        const table = document.querySelector('.table__body');
+        table.innerHTML = "";
+        
+        for (let j = 0; j < data.length; j++) {
+            const tableRow = document.createElement('div');
+            tableRow.classList.add('table__row');
+
+            for (let i = 0; i < Object.keys(data[j]).length; i++) {
+                const cell = document.createElement('span');
+                const massiv = [data[j].id, data[j].name, data[j].code, data[j].unit, data[j].count, data[j].price_purchase, data[j].price_selling];
+                cell.textContent = massiv[i];
+                tableRow.appendChild(cell);
+            }
+            table.appendChild(tableRow);
+        }
+    });
 });
 //------ //событие нажатия на кнопку 'ОК' в модалке списания ------
